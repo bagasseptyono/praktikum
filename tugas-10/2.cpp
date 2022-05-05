3718f6fea3bbec322adf12c1d45e26e74c7ff721
bagasseptyono <muhammadbagasmbs@gmail.com>
2022-05-05 10:42:50 +0700

    add tugas-10/2.cpp

---
+++ tugas-10/2.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int faktorial(int nilai);
+int faktorial(int nilai){
+    int hasil;
+    
+    if (nilai > 1) {
+        hasil = nilai * faktorial(nilai-1);
+    }else {
+        hasil = nilai;
+    }
+    
+    return hasil;
+    
+
+}
+
+
+int main (){
+    int input,total;
+    printf("Program menghitung faktorial\n");
+    printf("Masukkan bilangan : \n");
+    scanf("%i",&input);
+    total = faktorial(input);
+    printf("Hasil faktorial %i adalah %i",input, total);
+
+
+
+
+}
